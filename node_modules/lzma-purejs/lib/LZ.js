562937fcbaa87b0d0d65ef0c5b09d98b8058b9dc
polmina <pol.minana.ruiz@gmail.com>
2021-07-06 11:22:36 +0200

    init

--- node_modules/lzma-purejs/lib/LZ.js
+++
@@ -1,9 +0,0 @@
-if (typeof define !== 'function') { var define = require('amdefine')(module); }
-define(['./freeze','./LZ/BinTree','./LZ/InWindow','./LZ/OutWindow'],function(freeze,BinTree,InWindow,OutWindow){
-  'use strict';
-  return freeze({
-    BinTree: BinTree,
-    InWindow: InWindow,
-    OutWindow: OutWindow
-  });
-});
--- node_modules/lzma-purejs/lib/LZMA.js
+++
@@ -1,8 +0,0 @@
-if (typeof define !== 'function') { var define = require('amdefine')(module); }
-define(['./freeze', './LZMA/Decoder', './LZMA/Encoder'],function(freeze, Decoder, Encoder){
-  'use strict';
-  return freeze({
-    Decoder: Decoder,
-    Encoder: Encoder
-  });
-});
